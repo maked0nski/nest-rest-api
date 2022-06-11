080bbc932de96ee4e050c41c08131f507923e159
maked0nski <maked0nskihtc@gmail.com>
2022-06-11 16:43:02 +0300

    add swagger, add .env config files

--- src/alchemy-api/alchemy-api.service.ts
+++ src/alchemy-api/alchemy-api.service.ts
@@ -2,34 +2,54 @@ import {Injectable} from '@nestjs/common';
 import {HttpService} from "@nestjs/axios";
 import {map, Observable} from "rxjs";
 
-import {createAlchemyWeb3} from "@alch/alchemy-web3";
 import * as solanaWeb3 from '@solana/web3.js';
+
+const crypto = require('crypto');
 
 @Injectable()
 export class AlchemyApiService {
 
-    constructor(private httpService: HttpService) {
+    private readonly connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
+
+    constructor(private httpService: HttpService) {}
+
+    findAll(owner: string): Observable<any> {
+
+        let url = `${process.env.ALCHEMY_BASE_URL}/${process.env.ALCHEMY_API_KEY}/getNFTs?owner=${owner}`;
+
+        return this.httpService.get(url)
+            .pipe(
+                map(response => response.data.nfts)
+            )
     }
 
-    findAll(): Observable<any> {
 
-        let url = `${process.env.ALCHEMY_BASE_URL}/${process.env.ALCHEMY_API_KEY}/getNFTs/?owner=${process.env.OWNER}`;
 
+    GetAllTockenBySolana(account: string): Observable<any> {
+        let url = `${process.env.SOLANA_BASE_URL}/account/tokens?account=${account}`;
+        // console.log(url);
         return this.httpService.get(url)
             .pipe(
                 map(response => response.data)
             )
     }
 
+    async solana(_account: string) {
+        let solanaWallet = new solanaWeb3.PublicKey(_account);
 
-    async getNftByOwner(owner: String) {
-        const web3 = createAlchemyWeb3(
-            `${process.env.ALCHEMY_BASE_URL}/${process.env.ALCHEMY_API_KEY}`
+        // console.log('solanaWallet.encode()', solanaWallet.encode());
+        // console.log("solanaWallet-", solanaWallet);
+        // console.log("toBase58", solanaWallet.toBase58());
+
+        let connection = new solanaWeb3.Connection(
+            solanaWeb3.clusterApiUrl('devnet'),
+            'confirmed',
         );
-        return await web3.alchemy.getNfts({owner: `${owner}`})
+
+        let account = await connection.getAccountInfo(solanaWallet);
+        console.log("account", account);
+
+        return account
     }
 
-    async solana() {
-        return solanaWeb3
-    }
 }
